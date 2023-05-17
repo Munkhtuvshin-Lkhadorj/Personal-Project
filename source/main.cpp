f094974f56d7ee1a00b30b3e2b224eb23cde7902
Nick Lkhadorj <tlkhadorj25@my.whitworth.edu>
2023-05-17 00:40:03 -0700

    component selection

--- source/main.cpp
+++ source/main.cpp
@@ -3,78 +3,167 @@
 #include <string>
 #include <vector>
 #include <sstream>
+
 using namespace std;
 
-string motherboardSocketType;
+class Component {
+public:
+    string name;
+    string socketType;
+    string ramType;
+    string property1; // Add properties as needed
+    string property2;
+    Component() = default;
 
-string motherboardRamType;
-string motherboardStorageType;
-string cpuSocketType;
-string cpuRAMType;
-int cpuWattage;
-string ramType;
-int coolerWattage;
-string storageType;
-int gpuWattage;
-int powerSupplyMinWattage;
-int powerSupplyMaxWattage;
+    Component(const string& name, const string& socketType, const string& ramType)
+        : name(name), socketType(socketType), ramType(ramType) {
+    }
+};
 
-bool parseCSVFile(const string& filename, vector<string>& componentList){
+bool parseCSVFile(const string& filename, vector<Component>& componentList) {
     ifstream file(filename);
-    if (!file.is_open()){
-        cout<<"The failed didn't open correctly"<<endl;
+    if (!file.is_open()) {
+        cout << "Failed to open the file." << endl;
         return false;
-
     }
+
     string line;
-    while (getline(file, line)){
-        componentList.push_back(line);
+    getline(file, line);  // Skip the header row
 
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string category, name, socketType, ramType;
+        getline(ss, category, ',');
+        getline(ss, name, ',');
+        getline(ss, socketType, ',');
+        getline(ss, ramType, ',');
+
+        Component comp;
+        comp.name = name;
+        comp.socketType = socketType;
+        comp.ramType = ramType;
+
+        componentList.push_back(comp);
     }
+
     file.close();
     return true;
-
-
 }
-int main(){
-    vector<string> motherboardComponents;
-    vector<string> cpuComponents;
-    vector<string> ramComponents;
-    vector<string> coolerComponents;
-    vector<string> storageComponents;
-    vector<string> gpuComponents;
-    vector<string> powerSupplyComponents;
 
-    parseCSVFile("components.csv", motherboardComponents);
-    parseCSVFile("components.csv", cpuComponents);
-    parseCSVFile("components.csv", ramComponents);
-    parseCSVFile("components.csv", coolerComponents);
-    parseCSVFile("components.csv", storageComponents);
-    parseCSVFile("components.csv", gpuComponents);
-    parseCSVFile("components.csv", powerSupplyComponents);
-    
-    bool parseSuccess = true;
-
-    parseSuccess = parseCSVFile("components.csv", motherboardComponents);
-    if (!parseSuccess) {
-        // Handle error or exit the program
+void displayComponents(const vector<Component>& components) {
+    for (size_t i = 0; i < components.size(); ++i) {
+        cout << i + 1 << ". " << components[i].name << endl;
     }
+}
 
-    parseSuccess = parseCSVFile("components.csv", cpuComponents);
-    if (!parseSuccess) {
-        // Handle error or exit the program
-    }
+Component selectComponent(const vector<Component>& components) {
+    int choice;
+    cout << "Enter the number corresponding to your choice: ";
+    cin >> choice;
 
-    // Repeat the above steps for each component type
-
-    // Check if all parsing operations were successful
-    if (parseSuccess) {
-        // Parsing completed successfully
-        cout << "Parsing completed successfully." << endl;
+    // Subtract 1 to convert from user choice to vector index
+    int index = choice - 1;
+    if (index >= 0 && index < components.size()) {
+        return components[index];
     } else {
-        // Parsing failed for at least one component type
-        cout << "Parsing failed." << endl;
+        // Invalid choice, return an empty component
+        return Component("", "", "");
     }
+}
+
+int main() {
+    vector<Component> motherboardComponents;
+    vector<Component> cpuComponents;
+    vector<Component> ramComponents;
+    vector<Component> coolerComponents;
+    vector<Component> storageComponents;
+    vector<Component> gpuComponents;
+    vector<Component> powerSupplyComponents;
+
+    bool parseSuccess = parseCSVFile("cpu.csv", cpuComponents);
+    if (!parseSuccess) {
+        cout << "Failed to parse the CSV file for CPU." << endl;
+        return 1;
+    }
+
+    parseSuccess = parseCSVFile("motherboard.csv", motherboardComponents);
+    if (!parseSuccess) {
+        cout << "Failed to parse the CSV file Motherboard." << endl;
+        return 1;
+    }
+    parseSuccess = parseCSVFile("Ram.csv", ramComponents);
+    if (!parseSuccess) {
+        cout << "Failed to parse the CSV Ram." << endl;
+        return 1;
+    }
+    parseSuccess = parseCSVFile("Cooler.csv", coolerComponents);
+    if (!parseSuccess) {
+        cout << "Failed to parse the CSV Cooler." << endl;
+        return 1;
+    }
+    parseSuccess = parseCSVFile("Storage.csv", storageComponents);
+    if (!parseSuccess) {
+        cout << "Failed to parse the CSV Storage." << endl;
+        return 1;
+    }
+    parseSuccess = parseCSVFile("Gpu.csv", gpuComponents);
+    if (!parseSuccess) {
+        cout << "Failed to parse the CSV GPU." << endl;
+        return 1;
+    }
+    parseSuccess = parseCSVFile("PowerSupply.csv", powerSupplyComponents);
+    if (!parseSuccess) {
+        cout << "Failed to parse the CSV PowerSupply." << endl;
+        return 1;
+    }
+
+
+    // Display the available components and let the user choose one from each category
+    cout << "Choose one component from each category:" << endl;
+
+    // Motherboard
+    cout << "Motherboard:" << endl;
+    displayComponents(motherboardComponents);
+    Component selectedMotherboard = selectComponent(motherboardComponents);
+    cout << endl;
+
+    // CPU
+    cout << "CPU:" << endl;
+    displayComponents(cpuComponents);
+    Component selectedCPU = selectComponent(cpuComponents);
+    cout << endl;
+
+    // RAM
+    cout << "RAM:" << endl;
+    displayComponents(ramComponents);
+    Component selectedRAM = selectComponent(ramComponents);
+    cout << endl;
+
+    // Cooler
+    cout << "Cooler:" <<
+    endl;
+    displayComponents(coolerComponents);
+    Component selectedCooler = selectComponent(coolerComponents);
+    cout << endl;
+    // Storage
+    cout << "Storage:" << endl;
+    displayComponents(storageComponents);
+    Component selectedStorage = selectComponent(storageComponents);
+    cout << endl;
+
+    // GPU
+    cout << "GPU:" << endl;
+    displayComponents(gpuComponents);
+    Component selectedGPU = selectComponent(gpuComponents);
+    cout << endl;
+
+    // Power Supply
+    cout << "Power Supply:" << endl;
+    displayComponents(powerSupplyComponents);
+    Component selectedPowerSupply = selectComponent(powerSupplyComponents);
+    cout << endl;
+
+    // Do something with the selected components...
 
     return 0;
-}
+    }
